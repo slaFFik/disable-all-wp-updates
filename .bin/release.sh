d464b0890c1f5f386d69d359c2f989c42ba54cb4
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2020-12-11 12:53:22 +1100

    Fix build script slug.

--- .bin/release.sh
+++ .bin/release.sh
@@ -4,7 +4,7 @@
 # The difference is that this script lives in the plugin's git repo & doesn't require an existing SVN repo.
 
 # main config
-PLUGINSLUG="wp-call-button"
+PLUGINSLUG="disable-all-wp-updates"
 CURRENTDIR=`pwd`
 MAINFILE="disable-all-wp-updates.php" # this should be the name of your main php file in the wordpress plugin
 SKIP_NPM=true
